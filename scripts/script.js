272db88c25dfb3d94ab9fe5d6c06a4bbbd82736a
arlekin1208 <arlekin12089@gmail.com>
2022-02-08 12:00:10 +0100

    add styles to form

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,12 @@
 const showBtn = document.getElementById("show-btn");
 const addContainer = document.getElementById("card-window");
 const closeBtn = document.getElementById("close");
-//Show add container block
+const saveBtn = document.getElementById("save-card");
 
+//Show add container block
 showBtn.addEventListener("click", () => addContainer.classList.add("show"));
 closeBtn.addEventListener("click", () => addContainer.classList.remove("show"));
+
+// Add new card
+
+saveBtn.addEventListener("click", () => {});
