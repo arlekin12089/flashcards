85d406d1f6cf68c74fb9600e9d9b3c9ba1b25fd6
arlekin1208 <arlekin12089@gmail.com>
2022-02-08 11:24:10 +0100

    add form create form structure, css, js

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const showBtn = document.getElementById("show-btn");
+const addContainer = document.getElementById("card-window");
+const closeBtn = document.getElementById("close");
+//Show add container block
+
+showBtn.addEventListener("click", () => addContainer.classList.add("show"));
+closeBtn.addEventListener("click", () => addContainer.classList.remove("show"));
