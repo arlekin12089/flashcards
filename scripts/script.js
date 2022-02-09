c26d7abb122ed79d89a57efac2e3b72c2bdac4da
arlekin1208 <arlekin12089@gmail.com>
2022-02-09 19:58:55 +0100

    fix styles, add remove card js

--- scripts/script.js
+++ scripts/script.js
@@ -5,24 +5,19 @@ const questionFront = document.getElementById('question');
 const answerBack = document.getElementById('answer');
 const saveBtn = document.getElementById('save-card');
 const errorMessage = document.querySelector('.error-message');
-const cardList = document.getElementById('card-list');
-const clearBtn = document.getElementById('clear');
+const cardWrapper = document.getElementById("card-list");
+const clearBtn = document.getElementById("clear");
 const search = document.getElementById("search");
 
 // Store DOM cards
-const cardsList = [];
+let cardsList = [];
+
+// Store card info
+let cardsInfo = getCardsData();
 
 //Show add container block
 showBtn.addEventListener("click", () => addContainer.classList.add("show"));
 closeBtn.addEventListener("click", () => addContainer.classList.remove("show"));
-
-// Store cards
-const cardsInfo = getCardsInfo();
-
-// Create all cards
-function createCards() {
-  cardsInfo.forEach((info, index) => createCard(info, index));
-}
 
 // Create a single card in DOM
 function createCard(info, index) {
@@ -31,76 +26,78 @@ function createCard(info, index) {
 
   card.innerHTML = `
     <div class="card-container">
-  <div class="inner-card-front">
-	<h2 class="mb20">Question:</h2>
-    <p class="card-front">
-      ${info.front}
-    </p>
-	 </div>
-	 <div class="inner-card-back">
-	 <h2 class="mb20">Answer:</h2>
-    <p class="card-back">
-      ${info.back}
-    </p>
-  </div>
-  <a href="#" id="delete"><i class="far fa-trash-alt" id="remove-button"></i></a>
-  <a href="#" id="flip"><i class="fas fa-redo-alt" id="flip-button"></i></a>
-</div>
+  		<div class="inner-card-front">
+			<h2 class="mb20">Question:</h2>
+			<p class="card-front">
+			${info.front}
+			</p>
+	 	</div>
+	 	<div class="inner-card-back">
+	 		<h2 class="mb20">Answer:</h2>
+			<p class="card-back">
+			${info.back}
+			</p>
+  		</div>
+		<div class="actions">
+			<a href="#" id="delete" class="delete"><i class="far fa-trash-alt" id="remove-button"></i></a>
+			<a href="#" id="flip"><i class="fas fa-redo-alt" id="flip-button"></i></a>
+		</div>
+	</div>
   `;
   card.addEventListener("click", () => card.classList.toggle("show-answer"));
+
   // Add to DOM cards
   cardsList.push(card);
-  cardList.appendChild(card);
+  cardWrapper.appendChild(card);
+
+  const deleteCard = card.getElementsByClassName("delete");
+  deleteCard[0].addEventListener("click", (e) => {
+    cardsList.splice(index, 1);
+    cardWrapper.removeChild(card);
+    cardsInfo.splice(index, 1); //remove from localStorage data
+    setCardsData(cardsInfo); //update Local storage
+  });
 }
 
 // Get cards from local storage
-function getCardsInfo() {
+function getCardsData() {
   const cards = JSON.parse(localStorage.getItem("cards"));
   return cards === null ? [] : cards;
 }
 
 // Add card to local storage
-function setCardsInfo(cards) {
+function setCardsData(cards) {
   localStorage.setItem("cards", JSON.stringify(cards));
   window.location.reload();
 }
 
-createCards();
-
 // Add new card
 saveBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const front = questionFront.value;
-  const back = answerBack.value;
+  let front = questionFront.value;
+  let back = answerBack.value;
   if (front !== "" && back !== "") {
     const newCard = { front, back };
     createCard(newCard);
-    front.value = "";
-    back.value = "";
-    addContainer.classList.remove("show");
-    const deleteBtn = document.getElementById("delete");
-
-    // Clear cards button
-
-    deleteBtn.addEventListener("click", () => {
-      localStorage.clear();
-      cardList.innerHTML = "";
-      window.location.reload();
-    });
-
-    front.value = "";
-    back.value = "";
+    questionFront.value = "";
+    answerBack.value = "";
     addContainer.classList.remove("show");
     cardsInfo.push(newCard);
-    setCardsInfo(cardsInfo);
+    setCardsData(cardsInfo);
   } else {
     errorMessage.classList.add("show");
   }
 });
 
+// Create all cards
+function createCards() {
+  cardsInfo.forEach((info, index) => createCard(info, index));
+}
+createCards();
+
 // Clear cards button
-clearBtn.addEventListener('click', () => {
+clearBtn.addEventListener("click", () => {
   localStorage.clear();
-  cardList.innerHTML = '';
+  cardWrapper.innerHTML = "";
   window.location.reload();
 });
